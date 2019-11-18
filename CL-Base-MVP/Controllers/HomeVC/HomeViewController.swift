9641e7fe9ac55140695aaaae9bc5e7a1bc46e87a
RahulDhiman93 <rsdm34@gmail.com>
2019-11-18 18:26:32 +0530

    Alert with options added

--- CL-Base-MVP/Controllers/HomeVC/HomeViewController.swift
+++ CL-Base-MVP/Controllers/HomeVC/HomeViewController.swift
@@ -13,8 +13,15 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+            self.showAlert()
+        })
         // Do any additional setup after loading the view.
+    }
+    
+    private func showAlert() {
+        let alertVc = AlertWithOptionsViewController.loadNibView(alertTitle: "Thank you!", alertBody: "Thank you for installing our app.\nThanks again", leftButtonTitle: "okayqq", rightButtonTitle: "yayaya")
+        self.present(alertVc, animated: true, completion: nil)
     }
     
 }
---
+++ CL-Base-MVP/View/CustomAlert/AlertWithOptionsViewController.swift
@@ -0,0 +1,110 @@
+//
+//  AlertWithOptionsViewController.swift
+//  Inforu
+//
+//  Created by Rahul Dhiman on 18/11/19.
+//  Copyright © 2019 Deepak. All rights reserved.
+//
+
+import UIKit
+
+class AlertWithOptionsViewController: UIViewController {
+    
+    @IBOutlet weak var mainStackView: UIStackView!
+    @IBOutlet weak var alertTitle: UILabel!
+    @IBOutlet weak var alertBody: UILabel!
+    @IBOutlet weak var buttonsView: UIView!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+
+    var alertTitleText = ""
+    var alertBodyText = ""
+    var leftButtonTitle = ""
+    var rightButtonTitle = ""
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: .now()
+            + 0.05, execute: {
+            self.setupView()
+        })
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    private var leftButtonCallback: ((_ success: Bool) -> Void)?
+    private var rightButtonCallback: ((_ success: Bool) -> Void)?
+    
+    func leftButtonCallback(callback: @escaping ( _ success: Bool) -> Void) {
+        self.leftButtonCallback = callback
+    }
+    
+    func rightButtonCallback(callback: @escaping ( _ success: Bool) -> Void) {
+        self.rightButtonCallback = callback
+    }
+    
+    
+    class func loadNibView(alertTitle : String = "alert", alertBody : String = "" , leftButtonTitle : String = "cancel", rightButtonTitle : String = "okay") -> AlertWithOptionsViewController {
+        let vc = AlertWithOptionsViewController(nibName: "AlertWithOptionsViewController", bundle: nil)
+        vc.modalPresentationStyle = .overFullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        vc.configAlert(alertTitle: alertTitle, alertBody: alertBody, leftButtonTitle: leftButtonTitle, rightButtonTitle: rightButtonTitle)
+        return vc
+    }
+    
+    func configAlert(alertTitle : String = "alert", alertBody : String , leftButtonTitle : String = "cancel", rightButtonTitle : String = "okay") {
+        self.alertTitleText = alertTitle
+        self.alertBodyText = alertBody
+        self.leftButtonTitle = leftButtonTitle
+        self.rightButtonTitle = rightButtonTitle
+    }
+    
+    private func setupView() {
+        self.alertTitle.text = self.alertTitleText
+        self.alertBody.text = self.alertBodyText
+        self.leftButton.setTitle(self.leftButtonTitle, for: .normal)
+        self.rightButton.setTitle(self.rightButtonTitle, for: .normal)
+        self.setAnimations(isContentHidden: false)
+    }
+    
+    private func setAnimations(isContentHidden : Bool) {
+        UIView.animate(withDuration: 0.2) {
+            self.alertTitle.isHidden = isContentHidden
+            self.alertBody.isHidden = isContentHidden
+            self.buttonsView.isHidden = isContentHidden
+            self.view.layoutIfNeeded()
+        }
+        self.view.layoutIfNeeded()
+    }
+    
+    @IBAction func leftButtonTapped(_ sender: UIButton) {
+        self.setAnimations(isContentHidden: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+            self.dismiss(animated: true) {
+                self.leftButtonCallback?(true)
+            }
+        })
+       
+    }
+    
+    @IBAction func rightButtonTapped(_ sender: UIButton) {
+        self.setAnimations(isContentHidden: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+            self.dismiss(animated: true) {
+                self.rightButtonCallback?(true)
+            }
+        })
+    }
+    
+}
